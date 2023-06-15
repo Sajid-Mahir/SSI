39434b8e40af7b3d0482104e4e3ab212b6763257
Yeasin <myeasin49@gmail.com>
2023-06-15 18:00:32 +0600

    FIX: pushed aca-py and wallet folder was empty. Solved using remaning them

---
+++ aries-mobile-agent-react-native-backup/packages/legacy/core/configs/ledgers/indy/index.ts
@@ -0,0 +1,16 @@
+// import { IndyPoolConfig } from '@aries-framework/core'
+
+// // @ts-ignore
+// import ledgers from './ledgers.json'
+
+// export default ledgers as IndyPoolConfig[]
+
+// import BcGovTestNet from './bcgov-test-net/pool-config'
+import BSovrinTestNet from './bcovrin-test-net/pool-config'
+
+// import IndicioTestNet from './indicio-test-net/pool-config'
+// import SovrinStagingNet from './sovrin-staging-net/pool-config'
+
+// export default [IndicioTestNet, SovrinStagingNet,BCGovTestNet]
+
+export default [BSovrinTestNet]
